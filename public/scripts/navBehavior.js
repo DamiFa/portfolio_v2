7aee1c568c94a2860eaf810455acb5fc0085fc90
Dami Fa <fougi2@gmail.com>
2018-05-13 20:27:48 +0200

    move one line but feels better now

--- public/scripts/navBehavior.js
+++ public/scripts/navBehavior.js
@@ -7,6 +7,14 @@ document.addEventListener('DOMContentLoaded',function() {
     setSelectedProject();
 },false);
 
+function fillProjectIcons (){
+    let iconElements = document.querySelectorAll(".icon");
+
+    iconElements.forEach(iconElement => {
+        projectIcons[iconElement.id] = iconElement;
+    });
+}
+
 function getLastUrlPart (){
     let urlPath = window.location.pathname;
     let partsConnector = "/";
@@ -18,25 +26,12 @@ function getLastUrlPart (){
     return lastUrlPart;
 }
 
-function fillProjectIcons (){
-    let iconElements = document.querySelectorAll(".icon");
-
-    iconElements.forEach(iconElement => {
-        projectIcons[iconElement.id] = iconElement;
-    });
-}
-
 function setSelectedProject (){
     if(currentProjectSelected !== "") {
         assignClassToProject ();
     }
 }
 
-/**
- * Loop dans les icones et compare si le projet de l'iteration est le currentProjectSlected.
- * Si c'est le cas, il lui donne la classe icon-selected.
- * Sinon il lui donne la classe icon-unselected.
- */
 function assignClassToProject (){
     for(var icon in projectIcons){
         if(icon === currentProjectSelected) projectIcons[icon].classList.add("icon-selected");
